bb46d6502c4610ab2232c1f095a484b45c9d79f6
Marcos <marcossilvano496@gmail.com>
2017-02-25 14:24:30 -0300

    Added entities of OMDba Api

--- app/src/main/java/marcos/movieapp/data/entities/SearchResponse.java
+++ app/src/main/java/marcos/movieapp/data/entities/SearchResponse.java
@@ -1,4 +1,42 @@
 package marcos.movieapp.data.entities;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class SearchResponse {
+    @SerializedName("Search")
+    @Expose
+    private List<Search> searches = null;
+    @SerializedName("totalResults")
+    @Expose
+    private String totalResults;
+    @SerializedName("Response")
+    @Expose
+    private String response;
+
+    public List<Search> getSearches() {
+        return searches;
+    }
+
+    public void setSearches(List<Search> searches) {
+        this.searches = searches;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
 }
